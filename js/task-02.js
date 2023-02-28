add10c2e7fb0ed98f0b4ef3f47f2df280d53188e
Danil Deineka <tripledpadik@gmail.com>
2023-02-28 14:31:14 +0200

    fix 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -7,13 +7,46 @@ const ingredients = [
   "Condiments",
 ];
 
+// 1 variant 
+
+// ingredients.forEach((ingredient) => {
+//   const list = document.querySelector("#ingredients");
+//   const listItem = document.createElement("li");
+
+//   listItem.textContent = ingredient;
+
+//   listItem.classList.add("item");
+
+//   list.append(listItem);
+// });
+
+// 2 variant 
+
+// ingredients.map((ingredient) => {
+//   const list = document.querySelector("#ingredients");
+//   const listItem = document.createElement("li");
+
+//   listItem.textContent = ingredient;
+//   list.append(listItem);
+  
+//   listItem.classList.add("item");
+// });
+
+
+
+// 3 variant 
+
+const list = document.querySelector("#ingredients");
+
+const itemArray = [];
+
 ingredients.forEach((ingredient) => {
-  const list = document.querySelector("#ingredients");
   const listItem = document.createElement("li");
-
   listItem.textContent = ingredient;
-
   listItem.classList.add("item");
-
-  list.append(listItem);
+  itemArray.push(listItem);
 });
+
+console.log(...itemArray);
+
+list.append(...itemArray)
